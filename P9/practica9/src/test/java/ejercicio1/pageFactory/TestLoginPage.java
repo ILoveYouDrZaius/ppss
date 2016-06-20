35d099cbd27370e0f7a384204fabe7df7a2b585d
ILoveYouDrZaius <iloveyoudoctorzaius@gmail.com>
2016-06-20 10:42:07 +0200

    Primera versión de práctica 9

---
+++ P9/practica9/src/test/java/ejercicio1/pageFactory/TestLoginPage.java
@@ -0,0 +1,46 @@
+package ejercicio1.pageFactory;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class TestLoginPage {
+    WebDriver driver;
+    LoginPage poLogin;
+    ManagerPage poManagerPage;
+    
+    @Before
+    public void setup(){
+        driver = new FirefoxDriver();
+        poLogin = PageFactory.initElements(driver, LoginPage.class);
+    }
+    
+    @Test
+    public void test_Login_Correct(){
+        String loginPageTitle = poLogin.getLoginTitle();
+        Assert.assertTrue(loginPageTitle.toLowerCase().contains("guru99 bank"));
+        poLogin.login("mngr34733", "AbEvydU");
+        poManagerPage = PageFactory.initElements(driver, ManagerPage.class);
+        Assert.assertTrue(poManagerPage.getHomePageDashboardUserName().toLowerCase().contains("manger id : mngr34733"));
+        driver.close();
+    }
+    
+    @Test
+    public void test_Login_Incorrect(){
+        String loginPageTitle = poLogin.getLoginTitle();
+        Assert.assertTrue(loginPageTitle.toLowerCase().contains("guru99 bank"));
+        poLogin.login("juan", "juan");
+        
+        Alert alert = driver.switchTo().alert();
+        String mensaje = alert.getText();
+        Assert.assertEquals(mensaje, "User or Password is not valid");
+
+        alert.accept();
+        driver.close();
+    }
+
+}
